bc8ca7c37952b73a77326230899f55f0d1fe826b
stefani2002 <98656153+stefani2002@users.noreply.github.com>
2022-03-23 13:46:50 +0100

    Add files via upload

---
+++ HomeworkTask1/HomeworkTask1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace HomeworkTask1
+{
+    class Program
+    {
+        static void FunWithStrings(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            string reversed = String.Empty;
+
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                reversed += charArray[i];
+            }
+            Console.WriteLine($"Your input reversed is: {reversed}");
+
+    
+            int sumOfWovels = 0;
+            for(int i = 0; i < charArray.Length; i++)
+            {
+                if(charArray[i]=='a'|| charArray[i] == 'e'||charArray[i] == 'i'|| charArray[i] == 'o'|| charArray[i] == 'u')
+                {
+                    sumOfWovels++;
+                }
+            }
+            Console.WriteLine($"The number of wovels is: {sumOfWovels}");
+
+            if (word == reversed)
+            {
+                Console.WriteLine("Your input is a palindrome! :)");
+            }
+            else
+            {
+                Console.WriteLine("Your input is not a palindrome! :(");
+            }
+
+
+
+            string[] splited = word.Split(' ');
+
+            int counter = 0;
+            for (int i = 0; i < splited.Length; i++)
+            {
+                counter++;
+                
+            }
+            Console.WriteLine($"Your string has {counter} words!");
+
+
+            string LongestWord = "";
+            string ShortestWord = "";
+            int counter1 = 0;
+            foreach (String s in splited)
+            {
+                if (s.Length > counter1)
+                {
+                    LongestWord = s;
+                    counter1 = s.Length;
+                }
+            }
+            
+
+            foreach (String s in splited)
+            {
+                if (s.Length < counter1)
+                {
+                    ShortestWord = s;
+                    counter1 = s.Length;
+                }
+            }
+
+            Console.WriteLine($"The longest word in your string is: {LongestWord} ");
+            Console.WriteLine($"The shortest word in your string is: {ShortestWord} ");
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========TASK 1============");
+            Console.WriteLine("\nInput a string: ");
+            string userInput = Console.ReadLine().ToLower();
+            FunWithStrings(userInput);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ HomeworkTask1/HomeworkTask2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+
+namespace HomeworkTask2
+{
+    
+
+    class Program
+    {
+        static string removeExtraSpace(string example)
+        {
+            string trimmed = String.Join(" ", example.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries));
+
+            return trimmed;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string example = " The    best  Lorem  Ipsum        Generator in all the  sea!   Heave this   scurvy copyfiller fer yar         next   adventure  and cajol yar clients   into walking the plank with ev'ry layout!    Configure       above, then get yer pirate ipsum...own the high seas,    argh!";
+            Console.WriteLine(removeExtraSpace(example));
+
+
+            Console.ReadLine();
+        }
+    }
+}
